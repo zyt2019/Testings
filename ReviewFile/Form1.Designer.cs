e6e2f85a621a7ccce728693f4045957dd83e3a2f
zyt <469167991@qq.com>
2020-02-18 22:29:46 +0800

    文件的读和写

--- ReviewFile/Form1.Designer.cs
+++ ReviewFile/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.载入文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存到文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 57);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(917, 219);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.载入文件ToolStripMenuItem,
+            this.保存到文件ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(941, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 载入文件ToolStripMenuItem
+            // 
+            this.载入文件ToolStripMenuItem.Name = "载入文件ToolStripMenuItem";
+            this.载入文件ToolStripMenuItem.Size = new System.Drawing.Size(76, 21);
+            this.载入文件ToolStripMenuItem.Text = "载入文件&A";
+            this.载入文件ToolStripMenuItem.Click += new System.EventHandler(this.载入文件ToolStripMenuItem_Click);
+            // 
+            // 保存到文件ToolStripMenuItem
+            // 
+            this.保存到文件ToolStripMenuItem.Name = "保存到文件ToolStripMenuItem";
+            this.保存到文件ToolStripMenuItem.Size = new System.Drawing.Size(88, 21);
+            this.保存到文件ToolStripMenuItem.Text = "保存到文件&B";
+            this.保存到文件ToolStripMenuItem.Click += new System.EventHandler(this.保存到文件ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(941, 373);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 载入文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存到文件ToolStripMenuItem;
     }
 }
 
--- ReviewFile/Form1.cs
+++ ReviewFile/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,55 @@ namespace ReviewFile
         public Form1()
         {
             InitializeComponent();
+        }
+        public string filePath;
+        public string fileContent;
+        private void 保存到文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog=new SaveFileDialog())
+            {
+                saveFileDialog.Filter= "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+                saveFileDialog.RestoreDirectory = true;
+                if (saveFileDialog.ShowDialog()==DialogResult.OK)
+                {
+                    //using (StreamWriter sw=new StreamWriter(saveFileDialog.FileName,false,Encoding.Default,1024))
+                    //{
+                    //    sw.Write(richTextBox1.Text);
+                    //    sw.Flush();
+                    //}
+                    //
+                    using (FileStream fs = new FileStream(saveFileDialog.FileName,FileMode.OpenOrCreate))
+                    {
+                        string temp = richTextBox1.Text;
+                        byte[] data = Encoding.UTF8.GetBytes(temp);
+                        fs.Write(data, 0, data.Length);
+                    }
+                }
+            }
+        }
+
+        private void 载入文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog=new OpenFileDialog())
+            {
+                //openFileDialog.InitialDirectory = "C:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+                if (openFileDialog.ShowDialog()==DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    //读取文件到流
+                    Stream stream= openFileDialog.OpenFile();
+                    using (StreamReader reader=new StreamReader(stream,Encoding.UTF8))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        
+                    }
+                    richTextBox1.Text = fileContent;
+                }
+            }
         }
     }
 }
