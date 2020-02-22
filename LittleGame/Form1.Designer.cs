fcaa52b0169645836ceb9a91263ba4c842e9f119
zyt <469167991@qq.com>
2020-02-22 13:52:16 +0800

    增加了一个小游戏 打字练习

---
+++ LittleGame/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace LittleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.difficultyProcessBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel5,
+            this.difficultyProcessBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 136);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(68, 20);
+            this.toolStripStatusLabel1.Text = "Correct:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(46, 20);
+            this.toolStripStatusLabel2.Text = "Miss:";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(50, 20);
+            this.toolStripStatusLabel3.Text = "Total:";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(79, 20);
+            this.toolStripStatusLabel4.Text = "Accuracy:";
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(440, 20);
+            this.toolStripStatusLabel5.Spring = true;
+            this.toolStripStatusLabel5.Text = "Difficulty";
+            this.toolStripStatusLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // difficultyProcessBar
+            // 
+            this.difficultyProcessBar.Maximum = 701;
+            this.difficultyProcessBar.Name = "difficultyProcessBar";
+            this.difficultyProcessBar.Size = new System.Drawing.Size(100, 18);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("Rockwell Condensed", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 141;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(800, 136);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 162);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.ToolStripProgressBar difficultyProcessBar;
+    }
+}
+
---
+++ LittleGame/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LittleGame
+{
+    public partial class Form1 : Form
+    {
+        Status status = new Status();
+        private bool Start;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Random random;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Start = true;
+            random = new Random();
+            timer1.Interval = 800;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Start)
+            {
+                listBox1.Items.Add("Gamestart");                
+                Start = false;
+                return;
+            }
+            if (listBox1.Items.Contains("Gamestart"))
+            {
+                listBox1.Items.Clear();
+            }
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if (listBox1.Items.Count>7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("GameOver");
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                {
+                    timer1.Interval -= 10;
+                }
+                else if (timer1.Interval > 250)
+                {
+                    timer1.Interval -= 7;
+                }
+                else if (timer1.Interval<250)
+                {
+                    timer1.Stop();
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add("You Win!");
+                }
+                difficultyProcessBar.Value = 800 - timer1.Interval;
+                status.Update(true);
+            }
+            else
+            {
+                status.Update(false);
+            }
+            ShowScore(status);
+        }
+
+        private void ShowScore(Status stat)
+        {
+            status = stat;
+            toolStripStatusLabel1.Text = "Correct:" + stat.correct.ToString();
+            toolStripStatusLabel2.Text = "Missed:" + stat.missed.ToString();
+            toolStripStatusLabel3.Text = "Total:" + stat.total.ToString();
+            toolStripStatusLabel4.Text = "Accuracy:" + stat.accuracy.ToString() + "%";
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            //当点击控件而且控件显示GameOver,重新开始游戏 并有3 2 1 倒计时
+            if (listBox1.Items.Contains("GameOver"))
+            {
+                ShowScore(new Status());
+                Start = true;
+                timer1.Start();
+            }
+        }
+    }
+    public class Status
+    {
+        public int total;
+        public int missed;
+        public int correct;
+        public int accuracy;
+        public void Update(bool correctKey)
+        {
+            total++;
+            if (correctKey)
+            {
+                correct++;
+            }
+            else
+            {
+                missed++;
+            }
+            accuracy = 100 * correct / total;
+        }
+    }
+    
+}
